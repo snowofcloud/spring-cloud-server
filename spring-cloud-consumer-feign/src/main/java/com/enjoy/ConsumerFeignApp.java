8a1e0088fb1f48409cc4da1ceb51607d62b9ea7b
xxq <xuxq1015@163.com>
2019-03-21 23:44:48 +0800

    Feign自带负载均衡，因为其内置了ribbon，可看测试：服务由轮询变成了随机

--- spring-cloud-consumer-feign/src/main/java/com/enjoy/ConsumerFeignApp.java
+++ spring-cloud-consumer-feign/src/main/java/com/enjoy/ConsumerFeignApp.java
@@ -1,13 +1,15 @@
 package com.enjoy;
 
+import com.fun.config.RibbonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-//@RibbonClient(name = "SPRING-CLOUD-PRODUCT", configuration = RibbonConfig.class)
+@RibbonClient(name = "SPRING-CLOUD-PRODUCT", configuration = RibbonConfig.class)
 @EnableFeignClients("com.enjoy.service")
 public class ConsumerFeignApp {
 
